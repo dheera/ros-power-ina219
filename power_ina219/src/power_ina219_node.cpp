dd844904af0687a951d47d1011139ff145aa3ca9
dheera <dheera@dheera.net>
2019-03-19 23:13:06 +0000

    fix

--- power_ina219/src/power_ina219_node.cpp
+++ power_ina219/src/power_ina219_node.cpp
@@ -6,22 +6,14 @@
  * working.
  */
 
-<<<<<<< HEAD:power_ina219/src/power_ina219_node.cpp
 #include <power_ina219/ina219_activity.h>
-=======
-#include <pwm_pca9685/pca9685_activity.h>
->>>>>>> 5423c42620c5f128b8c94a3f33da8a5fef6f9f58:pwm_pca9685/src/pwm_pca9685_node.cpp
 #include <csignal>
 
 int main(int argc, char *argv[]) {
     ros::NodeHandle* nh = NULL;
     ros::NodeHandle* nh_priv = NULL;
 
-<<<<<<< HEAD:power_ina219/src/power_ina219_node.cpp
     power_ina219::INA219Activity* activity = NULL;
-=======
-    pwm_pca9685::PCA9685Activity* activity = NULL;
->>>>>>> 5423c42620c5f128b8c94a3f33da8a5fef6f9f58:pwm_pca9685/src/pwm_pca9685_node.cpp
 
     ros::init(argc, argv, "power_ina219_i2c_node");
 
@@ -40,11 +32,7 @@ int main(int argc, char *argv[]) {
         return -2;
     }
 
-<<<<<<< HEAD:power_ina219/src/power_ina219_node.cpp
     activity = new power_ina219::INA219Activity(*nh, *nh_priv);
-=======
-    activity = new pwm_pca9685::PCA9685Activity(*nh, *nh_priv);
->>>>>>> 5423c42620c5f128b8c94a3f33da8a5fef6f9f58:pwm_pca9685/src/pwm_pca9685_node.cpp
 
     if(!activity) {
         ROS_FATAL("Failed to initialize driver");
